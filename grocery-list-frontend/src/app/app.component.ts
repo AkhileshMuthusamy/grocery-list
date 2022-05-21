8b74d2ecc94febaa45239ea65c901418e3014aea
Akhilesh Muthusamy <AkhileshM@outlook.in>
2022-05-21 10:12:30 -0400

    Frontend: Initial Commit

---
+++ grocery-list-frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rideco-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'grocery-list-frontend';
+}
